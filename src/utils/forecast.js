6757bf29ccccfe899e5545f8ef603b98f2f8951c
Gregory Eckhart <gregeckhart@gregoryartsimac.attlocal.net>
2019-05-24 11:02:01 -0400

    added high/low temps, spelling fix

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,8 +11,9 @@ const forecast = (latitude, longitude, callback) => {
             console.log(url)
         }
         else {
-            callback(undefined, " It is currently " + body.currently.temperature + " degrees out. "
-                       + "There is a " + body.currently.precipProbability + "% chance of rain.")
+            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. "
+                       + "There is a " + body.currently.precipProbability + "% chance of rain. High: " + body.daily.data[0].temperatureHigh +
+                        ". Low: " + body.daily.data[0].temperatureLow + ".")
         }
     })
 }
